f8c4957548217f32b1be6c7df0520f0b2132e578
Tunghsiao Liu <t@sparanoid.com>
2022-06-09 11:37:46 -0400

    feat: add type support

--- main.py
+++ main.py
@@ -7,20 +7,50 @@ r = httpx.get(
     timeout=10,
 )
 vtbs = r.json()
+
+r_vdb = httpx.get(
+    'https://vdb.vtbs.moe/json/list.json',
+    timeout=10,
+)
+vtbs_vdb = r_vdb.json()['vtbs']
+
+vdb_dict = {}
 vup = {}
 vup_desc = {}
 
 # Prepare JSON
+for user in vtbs_vdb:
+    pending_removal = True
+    uid = None
+    for account in user['accounts']:
+        if account.get('platform') == 'bilibili':
+            pending_removal = False
+            uid = account['id']
+    if not pending_removal:
+        default_name_lang = user['name']['default']
+        vdb_dict[uid] = dict(
+            name = user['name'][default_name_lang],
+            type = user['type'],
+        )
+
 for user in vtbs:
     if 'mid' in user:
         uid = user['mid']
         face_hash = urlparse(user['face']).path
+
+        if str(uid) in vdb_dict.keys():
+            type = vdb_dict[str(uid)]['type']
+        else:
+            type = 'unknown'
+
         vup[uid] = dict(
             name = user['uname'],
+            type = type,
             face = face_hash,
         )
         vup_desc[uid] = dict(
             name = user['uname'],
+            type = type,
             face = face_hash,
             sign = user['sign'],
         )
